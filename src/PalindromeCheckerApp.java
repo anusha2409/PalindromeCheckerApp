2cbf0a1e3cc4c66f6a2a11b5c989cd1b15b85aae
anusha2409 <ap1251@srmist.edu.in>
2026-02-26 09:00:36 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,20 @@
 public class PalindromeCheckerApp {
 
-        public static void main(String[] args) {
-            System.out.println("Welcome to Palindrome Checker App");
-            System.out.println("Version 1.0");
+    public static void main(String[] args) {
+
+        String input = "madam";   // Hardcoded string
+        boolean isPalindrome = true;
+
+        // Check palindrome using half-length comparison
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
         }
+
+        // Output
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? " + isPalindrome);
     }
+}
