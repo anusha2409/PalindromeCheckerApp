578192948e7339ad9072d8a8633db9c1b80c4630
anusha2409 <ap1251@srmist.edu.in>
2026-02-26 10:19:14 +0530

    UC11:Object-Oriented Palindrome Service

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,29 +1,29 @@
 public class PalindromeCheckerApp {
+        public static void main(String[] args) {
 
-    public static void main(String[] args) {
+            String input = "racecar";
+            PalindromeService service = new PalindromeService();
+            boolean result = service.checkPalindrome(input);
 
-        String input = "A man a plan a canal panama";
-
-        boolean result = isPalindrome(input);
-
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? " + result);
-    }
-
-    private static boolean isPalindrome(String input) {
-
-        String normalized = input
-                .replaceAll("\\s+", "")
-                .toLowerCase();
-        for (int i = 0; i < normalized.length() / 2; i++) {
-
-            if (normalized.charAt(i) !=
-                    normalized.charAt(normalized.length() - 1 - i)) {
-
-                return false;
-            }
+            System.out.println("Input : " + input);
+            System.out.println("Is Palindrome? " + result);
         }
-
-        return true;
     }
-}
+
+    class PalindromeService {
+        public boolean checkPalindrome(String input) {
+            int start = 0;
+            int end = input.length() - 1;
+            while (start < end) {
+
+                if (input.charAt(start) != input.charAt(end)) {
+                    return false;
+                }
+
+                start++;
+                end--;
+            }
+
+            return true;
+        }
+    }
